207b22e13c30bb04d5bb8f4c9ca8e0d7ebe838dc
Philip Bradbury <34314378+philipwebvue@users.noreply.github.com>
2023-09-04 15:06:35 +0100

    Update favicons

--- header.php
+++ header.php
@@ -22,9 +22,9 @@
     <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri() . "/assets/images/favicon/favicon-32x32.png" ?>" >
     <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri() . "/assets/images/favicon/favicon-16x16.png" ?>" >
     <link rel="manifest" href="<?php echo get_template_directory_uri() . "/assets/images/favicon/site.webmanifest" ?>" >
-    <link rel="mask-icon" href="<?php echo get_template_directory_uri() . "/assets/images/favicon/safari-pinned-tab.svg" ?>"  color="#164a84">
+    <link rel="mask-icon" href="<?php echo get_template_directory_uri() . "/assets/images/favicon/safari-pinned-tab.svg" ?>"  color="#900942">
     <link rel="shortcut icon" href="<?php echo get_template_directory_uri() . "/assets/images/favicon/favicon.ico" ?>" >
-    <meta name="msapplication-TileColor" content="#164a84">
+    <meta name="msapplication-TileColor" content="#ffc40d">
     <meta name="msapplication-config" content="<?php echo get_template_directory_uri() . "/assets/images/favicon/browserconfig.xml" ?>" >
     <meta name="theme-color" content="#ffffff">
 
